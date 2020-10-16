62a60f5479b25602dd2094f2d6d58faba512a2c8
Scheyk <kathrin.dostal@gmail.com>
2020-10-16 10:34:29 +0200

    the basic script

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let button = document.getElementById('button');
 let table = 10;
 
+button.addEventListener("click", clear);
 button.addEventListener("click", multiply);
 
-function multiply(input) {
+function multiply() {
     let inputFiel = document.getElementById('input').value;
 
     for (let i = 1; i <= table; i++) {
         document.getElementById('table').innerHTML += `${inputFiel} x ${i} = ` + (inputFiel * i) + '<br>';
     }
 
+}
+
+function clear() {
+    document.getElementById('table').innerHTML = "";
 }
