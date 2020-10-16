fef59a3ac1215673fb1ef69e0946e35e5252140d
Scheyk <kathrin.dostal@gmail.com>
2020-10-16 19:35:29 +0200

    get try and catch with many answers,litle bug by tiping some buchstaben

--- script2.js
+++ script2.js
@@ -1,33 +1,53 @@
 // variablen
 let button = document.getElementById('button');
-let table = 10;
+let multi = 10;
+
+//let table = document.getElementById('output');
+
+//let fill = document.createTextNode("");
+
 
 
 // eventlistener
 button.addEventListener("click", clear);
-button.addEventListener("click", multiply, false);
+button.addEventListener("click", multiply);
 
 
 // functions
 function multiply() {
-    let inputFiel = document.getElementById('input').value;
+    let inputFiel = Number(document.getElementById('input').value);
 
-    if (inputFiel === 0 || inputFiel > 10 || inputFiel === isNaN) {
+    if (inputFiel.value == '' || inputFiel == 0 || (inputFiel % 1) !== 0 || Number.isNaN(inputFiel) || inputFiel > 10 || inputFiel < 1) {
         try {
-            if (inputFiel === 0) throw "Every number multiplied by 0 is 0. You don’t need a table for that";
-            if (inputFiel > 10) throw "You should enter a number smaller than 10";
+            if (inputFiel.value == '') throw "you must enter a number...";
+            if (inputFiel == 0) throw "Every number multiplied by 0 is 0. You don’t need a table for that";
+            if ((inputFiel % 1) !== 0) throw "choose a full number. Not a decimal :D";
             if (isNaN(inputFiel)) throw "only numbers...";
+            if (inputFiel > 10) throw "You should enter a number smaller than 10";
+            if (inputFiel < 1) throw "a number between 1 and 10.NOT smaler ;)";
+
         } catch (error) {
-            document.getElementById('table').innerHTML = error;
+
+            document.getElementById('error').innerHTML = error;
         }
     } else {
-        for (i = 1; i < table; i++) {
-            document.getElementById('table').innerHTML += `${inputFiel} x ${i} =` + (inputFiel * i) + '<br>';
+
+        for (i = 1; i <= multi; i++) {
+
+            let newTr = document.createElement('tr');
+            let newTd = document.createElement('td');
+
+            newTd.innerHTML += `${inputFiel} x ${i} =` + (inputFiel * i);
+            newTr.appendChild(newTd);
+
+            document.getElementById('tBody').appendChild(newTr);
         }
+
     }
 
 }
 
 function clear() {
-    document.getElementById('table').innerHTML = "";
+    document.getElementById('error').innerHTML = "";
+    document.getElementById('tBody').innerHTML = "";
 }
