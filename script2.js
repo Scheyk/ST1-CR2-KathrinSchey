feae51f1f49f8ec2b97b24a9a2dbb21af9ed18b9
Scheyk <kathrin.dostal@gmail.com>
2020-10-17 08:32:16 +0200

    only bug now by empty input

--- script2.js
+++ script2.js
@@ -16,15 +16,17 @@ button.addEventListener("click", multiply);
 // functions
 function multiply() {
     let inputFiel = Number(document.getElementById('input').value);
+    console.log(inputFiel % 1);
 
-    if (inputFiel.value == '' || inputFiel == 0 || (inputFiel % 1) !== 0 || Number.isNaN(inputFiel) || inputFiel > 10 || inputFiel < 1) {
+    if (inputFiel === '' || Number(isNaN(inputFiel)) || (inputFiel % 1) !== 0 || inputFiel === 0 || inputFiel > 10 || inputFiel < 0) {
         try {
-            if (inputFiel.value == '') throw "you must enter a number...";
-            if (inputFiel == 0) throw "Every number multiplied by 0 is 0. You don’t need a table for that";
+
+            if (inputFiel === '') throw "you must enter a number...";
+            if (Number(isNaN(inputFiel))) throw "only numbers...";
+            if (inputFiel === 0) throw "Every number multiplied by 0 is 0. You don’t need a table for that";
             if ((inputFiel % 1) !== 0) throw "choose a full number. Not a decimal :D";
-            if (isNaN(inputFiel)) throw "only numbers...";
             if (inputFiel > 10) throw "You should enter a number smaller than 10";
-            if (inputFiel < 1) throw "a number between 1 and 10.NOT smaler ;)";
+            if (inputFiel < 0) throw "a number between 1 and 10.NOT smaler ;)";
 
         } catch (error) {
 
