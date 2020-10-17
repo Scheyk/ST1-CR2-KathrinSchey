c61086ced22c577b713d247ccb0c36a5b1549fb6
Scheyk <kathrin.dostal@gmail.com>
2020-10-17 11:26:04 +0200

    nor all finegit add script2.js

--- script2.js
+++ script2.js
@@ -15,22 +15,25 @@ button.addEventListener("click", multiply);
 
 // functions
 function multiply() {
-    let inputFiel = Number(document.getElementById('input').value);
-    console.log(inputFiel % 1);
+    let inputFiel = document.getElementById('input').value;
+    //console.log(inputFiel === "");
 
-    if (inputFiel === '' || Number(isNaN(inputFiel)) || (inputFiel % 1) !== 0 || inputFiel === 0 || inputFiel > 10 || inputFiel < 0) {
+    if (inputFiel === "" || isNaN(inputFiel) || (inputFiel % 1) !== 0 || inputFiel == 0 || inputFiel > 10 || inputFiel < 0) {
         try {
 
-            if (inputFiel === '') throw "you must enter a number...";
-            if (Number(isNaN(inputFiel))) throw "only numbers...";
-            if (inputFiel === 0) throw "Every number multiplied by 0 is 0. You don’t need a table for that";
+            if (inputFiel === "") throw "you must enter a number...";
+            if (isNaN(inputFiel)) throw "only numbers...";
             if ((inputFiel % 1) !== 0) throw "choose a full number. Not a decimal :D";
+            if (inputFiel == 0) throw "Every number multiplied by 0 is 0. You don’t need a table for that";
             if (inputFiel > 10) throw "You should enter a number smaller than 10";
             if (inputFiel < 0) throw "a number between 1 and 10.NOT smaler ;)";
 
         } catch (error) {
 
             document.getElementById('error').innerHTML = error;
+
+        } finally {
+            return false;
         }
     } else {
 
